ea1ec90b41d674ca5272c01cfa0058bac5ec99f3
mesyamaureen <mesyamaureen@gmail.com>
2023-10-17 21:28:57 +0200

    created hello world app

---
+++ src/main/java/htw/berlin/runnerslog/HelloWorld.java
@@ -0,0 +1,11 @@
+package htw.berlin.runnerslog;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+    @GetMapping("/")
+    public String index() {
+        return "Hello World!";
+    }
+}
