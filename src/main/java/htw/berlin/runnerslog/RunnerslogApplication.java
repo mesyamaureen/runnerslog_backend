61db4ac59964c02d9bbfd1d942fcc70d2ee90135
mesyamaureen <mesyamaureen@gmail.com>
2023-10-17 21:01:09 +0200

    initial commit

---
+++ src/main/java/htw/berlin/runnerslog/RunnerslogApplication.java
@@ -0,0 +1,13 @@
+package htw.berlin.runnerslog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnerslogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunnerslogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/htw/berlin/runnerslog/RunnerslogApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.berlin.runnerslog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnerslogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
